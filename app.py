2feb3e6e1af9779b9fbad2848266f87f9fe6c09f
kcheam001 <99530088+kcheam001@users.noreply.github.com>
2022-02-26 15:23:35 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -16,10 +16,14 @@ app = Flask(__name__)
 # In[3]:
 
 
-from flask import render_template, request
+from flask import render_template, request 
 import joblib
 
-@app.route ("/", methods = ["GET", "POST"])
+
+# In[4]:
+
+
+@app.route("/", methods = ["GET", "POST"])
 def index():
     if request.method == "POST":
         purchases = request.form.get("purchases")
@@ -32,26 +36,30 @@ def index():
         s1 = "The score of credit card upgrade based on decision tree is " + str(pred1)
         model2 = joblib.load("CCU_Regression")
         pred2 = model2.predict([[purchases,suppcard]])
-        s2 = "The score of credit card upgrade based on Regression is " + str(pred2)
+        s2 = "The score of credit card upgrade based on decision tree is " + str(pred2)
         model3 = joblib.load("CCU_NN")
         pred3 = model3.predict([[purchases,suppcard]])
-        s3 = "The score of credit card upgrade based on NN is " + str(pred3)
+        s3 = "The score of credit card upgrade based on decision tree is " + str(pred3)
         model4 = joblib.load("CCU_RandomForest")
         pred4 = model4.predict([[purchases,suppcard]])
-        s4 = "The score of credit card upgrade based on RF is " + str(pred4)
+        s4 = "The score of credit card upgrade based on decision tree is " + str(pred4)
         model5 = joblib.load("CCU_XGBoost")
         pred5 = model5.predict([[purchases,suppcard]])
-        s5 = "The score of credit card upgrade based on GB is " + str(pred5)
-        return(render_template("index2.html", result1 = s1 , result2 = s2, result3 = s3, result4 = s4, result5 = s5))
-    else: 
-        return(render_template("index2.html", result1 = "2", result2 = "2", result3 = "2", result4 = "2", result5 = "2"))
-    
-        
+        s5 = "The score of credit card upgrade based on decision tree is " + str(pred5)
+        return(render_template("index.html", result1=s1, result2=s2, result3=s3, result4=s4, result5=s5))
+    else:
+        return(render_template("index.html", result1="2", result2="2", result3="2", result4="2", result5="2"))
 
 
 # In[ ]:
 
 
-if __name__ == "__main__":
+if __name__ == "__main__" :
     app.run()
+
+
+# In[ ]:
+
+
+
 
